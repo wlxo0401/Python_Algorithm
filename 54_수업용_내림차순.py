56a8366245c6d9ffcc2ea23655591cd74833ff7b
kimjitae <wlxo0401@gmail.com>
2020-09-02 18:33:54 +0900

    수업

---
+++ 54_수업용_내림차순.py
@@ -0,0 +1,17 @@
+
+def first():
+    list = [12, 30, 6, 7, 13, 8, 11, 50, 24, 2, 5, 10]
+    list.sort(reverse=True)
+    print("라이브러리 사용 : ",list)
+
+def second():
+    list = [12, 30, 6, 7, 13, 8, 11, 50, 24, 2, 5, 10]
+    for i in range(0, len(list) - 1, 1):
+        for j in range(i + 1, len(list), 1):
+            if list[i] < list[j]:
+                temp = list[j]
+                list[j] = list[i]
+                list[i] = temp
+    print("라이브러리 사용 안함 : ", list)
+first()
+second()
